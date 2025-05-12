b84b974503eab0891c046f8af3ddfd41276aa829
luciamartino <luciamartino.lm98@gmail.com>
2025-05-12 09:28:40 +0200

    commit REACT

--- REACT/my-app/src/main.jsx
+++ REACT/my-app/src/main.jsx
@@ -1,7 +1,7 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
-import HelloWorld from './App.jsx'
+import HelloWorld from './HelloWorld.jsx'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
