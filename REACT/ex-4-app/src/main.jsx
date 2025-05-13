305206a1e33757d83d5f6ad9d1e04c4d1d6ddd9b
luciamartino <luciamartino.lm98@gmail.com>
2025-05-13 15:40:02 +0200

    ex-4 changes

--- REACT/ex-4-app/src/main.jsx
+++ REACT/ex-4-app/src/main.jsx
@@ -2,4 +2,10 @@ import { createRoot } from "react-dom/client"
 import { StrictMode } from "react"
 import { Welcome } from "./Welcome"
 
-createRoot(document.getElementById("root")).render(<Welcome name = "John" age ={27} />)
+createRoot(document.getElementById("root")).render(
+    <>
+        <StrictMode>
+            <Welcome name = "John" age ={27} />
+        </StrictMode>
+    </>
+)
