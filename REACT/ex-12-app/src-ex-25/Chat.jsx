ef43de623465801bb7b22c62f20a4eeb1416c04f
luciamartino <luciamartino.lm98@gmail.com>
2025-07-29 16:29:13 +0200

    last commit ex-25

--- REACT/ex-12-app/src-ex-25/Chat.jsx
+++ REACT/ex-12-app/src-ex-25/Chat.jsx
@@ -1,5 +1,6 @@
 import { useEffect, useState } from "react"
 import useSWR from "swr";
+import "./index.css"
 
 export function Chat(){
 
@@ -20,10 +21,9 @@ export function Chat(){
         if(message.length == 0){
             alert("no message in the box, are u alright?")
         }else{
-            setTexts([...texts, message]);
+            setTexts(prev => [...prev, {role: "user", content: message}]);
             setMessage("")
-
-            setTriggeredURL(`https://mocki.io/v1/c06704c7-db58-4435-a59c-480937866126`)
+            setTriggeredURL(`https://mocki.io/v1/1fd23cd5-ae96-4d9a-b699-f483f8b00145`)
             //send a message => handleSubmit triggers setTriggeredURL =>  useSWR notices the change => it refetch the new answer from the bot
             //anytime that the button is clicked => useSWR needs to fetch new data
 
@@ -49,17 +49,17 @@ export function Chat(){
     useEffect(() => {
         if(data?.response && !texts.includes(data.response)){
             //if there's a response in the data and this response is not already in the texts array
-            setTexts(prev => [...prev, data.response])
+            setTexts(prev => [...prev, {role: "bot", content: data.response}])
             //if you write setTexts([...texts, data.response]) => it doesn't check if there are duplicates in the texts
         }
-    }, [data, texts])
+    }, [data])
 
     return(
         <>
         <div className="chatroom">
             <ul>
                 {texts.map((msg, index) => ( //if you put {} it doesn't return anything
-                    <li key={index}>{msg.trim()}</li>
+                    <li key={index} className={msg.role === "user" ? "user-msg" : "bot-msg"}>{msg.content.trim()}</li>
                 ))}
             </ul>
             <form onSubmit={handleSubmit}>
@@ -75,7 +75,6 @@ export function Chat(){
 //message è quello che viene messo dentro l'input
 //texts è l'array dentro il quale vengono inseriti i messaggi precedenti, una volta inviati
 
-
 //if you want to move the alert behavior out of the render:
 // useEffect(() => {
 //   if (isLoading) {
@@ -88,3 +87,12 @@ export function Chat(){
 //       .then(res => res.text())
 //       .then(console.log);
 //   }, []);
+
+
+//That structure { role: "user", content: message } is a little data design magic ✨
+
+// Instead of storing just the raw string message in your texts array, you're now storing an object that includes:
+
+//     role: a label to say who the message is from—either "user" or "bot"
+
+//     content: the actual message text itself
